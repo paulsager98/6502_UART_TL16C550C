f0f916b65975638ba4f648c23ddf310aa9a591a8
paulsager98 <166425796+paulsager98@users.noreply.github.com>
2024-05-12 23:39:53 +0200

    Update test.s

--- test.s
+++ test.s
@@ -166,14 +166,12 @@ lcd_instruction:
 print_char:
   jsr lcd_wait
   sta PORTB
-  pha
   lda #RS         ; Set RS; Clear RW/E bits
   sta PORTA
   lda #(RS | E)   ; Set E bit to send instruction
   sta PORTA
   lda #RS         ; Clear E bits
   sta PORTA
-  pla
   rts
 
 
